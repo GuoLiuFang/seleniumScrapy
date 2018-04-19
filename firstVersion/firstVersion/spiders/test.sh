23c9df9174b2d916f5feec655800491ea910d857
3B-4-12  郭流芳 <guolf@glodon.com>
2018-04-19 10:23:23 +0800

    推出后台执行并发操作的业务，不知道是什么效果

--- firstVersion/firstVersion/spiders/test.sh
+++ firstVersion/firstVersion/spiders/test.sh
@@ -1,7 +1,14 @@
 start=1
-for end in `seq 100000 100000 500000`; do
-    echo "start=${start}========end=${end}"
-    echo "开始使用参数"
-    echo "变更参数"
+for end in `seq -f "%.0f" 100000 100000 3000000`;do
+    echo "start========${start}========end=${end}"
+    if [ `expr ${end} % 500000` == 0 ]
+    then
+        echo "这里执行阻塞任务"
+		scrapy crawl zwDetails -astartid="${start}" -astopid="${end}" -ahost="localhost" -o "job51-${start}-${end}".jl
+    else
+        echo "这里执行后台任务。。通过后台实现并发"
+		scrapy crawl zwDetails -astartid="${start}" -astopid="${end}" -ahost="localhost" -o "job51-${start}-${end}".jl &
+    fi
+    #最后执行更新操作
     start=$((1+${end}))
 done
